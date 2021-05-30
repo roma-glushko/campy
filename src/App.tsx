bc99bbee26725b58b536b29e84a44edb18b15ca6
Roman Glushko <roman.glushko.m@gmail.com>
2021-05-30 23:39:09 +0300

    Added ability to find max sample idx per class, inited a dialog box to select projectDir

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 const fs = require('fs');
 
 import './App.global.css';
@@ -15,15 +16,18 @@ class Hello extends React.Component {
     this.state = {
       cameraStreamMounted: false,
       className: 'rock',
-      sampleIdx: 116,
+      classNames: ['rock', 'paper', 'scissors'],
+      projectDir: '/Users/roman/Projects/kaggle/frtgnn-rock-paper-scissor/data/webcam/',
+      projectPrefix: 'glu_',
     }
+
+    this.getClassIndices(this.state.projectDir)
   }
 
   componentDidMount() {
     this.mountCameraStream(this.camera.current)
   }
 
-  // mount camera video stream to video element
   mountCameraStream = (cameraElement) => {
     cameraElement.msHorizontalMirror = true;
 
@@ -39,15 +43,53 @@ class Hello extends React.Component {
         this.setState({
           cameraStreamMounted: true,
         })
-      })
-      .catch((error) => {
+      }).catch((error) => {
         console.log("Something went wrong: " + error);
       })
   }
 
+  getClassIndices = (projectDir) => {
+    const { classNames } = this.state
+
+    classNames.forEach(className => {
+      console.log(className)
+
+      fs.readdir(`${projectDir}${className}`, (err, files) => {
+        console.log(files)
+        console.log(files.lastItem)
+        console.log(files.lastItem.match(/\d+/g)[0])
+        const lastSampleIndex = parseInt(files.lastItem.match(/\d+/g)[0]) + 1 // todo: handle no items/no numbers cases
+
+        this.setState(prevState => ({
+          'indices': {
+            ...prevState.indices,
+            [className]: lastSampleIndex,
+          },
+          'classStats': {
+            ...prevState.classStats,
+            [className]: files.length,
+          }
+        }))
+      })
+    });
+  }
+
+  selectProjectDir = () => {
+    const path = dialog.showOpenDialog({
+      properties: ['openDirectory']
+    })
+
+    console.log('directories selected', path)
+  }
+
   takePhoto = () => {
-    const sampleIdx = this.state.sampleIdx
-    const className = this.state.className
+    const { className, classStats, projectDir, projectPrefix, indices } = this.state
+
+    console.log(indices)
+    console.log(classStats)
+
+    const sampleIdx = indices[className]
+    const numClassSamples = classStats[className]
 
     const cameraElement = this.camera.current
     const canvasElement = this.canvas.current
@@ -55,15 +97,26 @@ class Hello extends React.Component {
     canvasElement.getContext('2d').drawImage(cameraElement, 0, 0, 300, 300);
 
     const photoData = canvasElement.toDataURL('image/png').replace(/^data:image\/(png|jpg|jpeg);base64,/, '');
-    const filePath = `/Users/roman/Projects/kaggle/frtgnn-rock-paper-scissor/data/webcam/${className}/glu_${sampleIdx}.png`
-    //const filePath = `/Users/roman/Projects/kaggle/frtgnn-rock-paper-scissor/data/noise/glu_${sampleIdx}.png`
+
+    const filePath = `${projectDir}${className}/${projectPrefix}${sampleIdx}.png`
 
     fs.writeFile(filePath, photoData, 'base64', (err) => {
       if (err) {
         alert(`There was a problem saving the photo: ${err.message}`);
       }
 
-      this.setState({sampleIdx: sampleIdx + 1})
+      console.log(`${filePath} has been created ✅`)
+
+      this.setState(prevState => ({
+        'indices': {
+          ...prevState.indices,
+          [className]: sampleIdx + 1,
+        },
+        'classStats': {
+          ...prevState.classStats,
+          [className]: numClassSamples + 1,
+        }
+      }))
     });
 
   }
@@ -76,12 +129,18 @@ class Hello extends React.Component {
           <canvas ref={this.canvas} width="300" height="300" style={{'display': 'none'}}></canvas>
           <video ref={this.camera} playsInline={true} autoPlay={true}></video>
         </div>
-        <select value={this.state.className} onChange={(event) => this.setState({className: event.target.value})}>
-          <option value="rock">Rock</option>
-          <option value="paper">Paper</option>
-          <option value="scissors">Scissors</option>
-        </select>
-        <button onClick={this.takePhoto}>Capture</button>
+        <div>
+          <div>
+            <input type="text" placeholder={this.state.projectDir} id="actual-file" disabled="disabled"/>
+            <input type="button" onClick={() => this.selectProjectDir()} value="Choose a file" id="select-file"/>
+          </div>
+          <div>
+            <select value={this.state.className} onChange={(event) => this.setState({className: event.target.value})}>
+              {this.state.classNames.map((className) => <option key={className} value={className}>{className}</option>)}
+            </select>
+          </div>
+          <button onClick={this.takePhoto}>Capture</button>
+        </div>
       </div>
     );
   }
